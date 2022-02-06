6d192b6d5a802b7f7d0f036be27d553ba61728f7
John Wells <molekewl2017@gmail.com>
2022-02-06 07:42:58 -0800

    add font / clean up index.js & css / Add a logo / create dummy data

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossorigin='true'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
